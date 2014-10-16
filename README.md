eee5630d73043edde11556350477bd0be6e94b21
Fernando Moreira <fernando.moreira.costa@gmail.com>
2014-10-16 15:17:17 -0300

    update badges to new and old list

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-Kanban-Projects
-===============
+# Kanban Projects
 
-Kanban Projects
+Kanban for projects views
+based in Bootstrap 3 and jQuery UI
+
+Work in progress!
+
+## License
+
+As long as you retain this notice you can do whatever you want with this stuff. If we meet some day, and you think this stuff is worth it, you can buy me a coffee in return.
+
+Fernando Moreira <f@nandomoreira.me>
--- js/main.js
+++ js/main.js
@@ -1,19 +1,46 @@
+/*!
+ * JQUERY UI - KANBAN PROJECTS
+ *
+ * @author Fernando Moreira <f@nandomoreira.me>;
+ * Licensed under the MIT license
+ */
 $(function() {
+	var kanban = $(".kanban .sortable-list"),
+		oldList, newList, item, newCount, oldCount,
+		message = $('.message');
 
-	$(".kanban .sortable-list").sortable({
+	message.hide();
+
+	kanban.sortable({
 		connectWith: '.sortable-list',
 		opacity: 0.6,
 		placeholder: "placeholder",
+		forcePlaceholderSize: true,
 		cancel: ".disabled",
+		help: "clone",
+		items: 'li',
+		start : function(event, ui) {
+			$(this).sortable("refresh");
+			item    = ui.item;
+			newList = oldList = ui.item.parent();
+		},
+		change : function(event, ui) {
+			if (ui.sender) {
+                newList = ui.placeholder.parent();
+            }
+		},
+		stop : function() {
+			newCount = newList.find('li').length;
+			oldCount = oldList.find('li').length;
+
+			// update badges to new and old list
+			newList.parent().find('.badge').html( newCount );
+			oldList.parent().find('.badge').html( oldCount );
+
+			message.fadeIn(200);
+			message.find('.msg').html("Moved <strong>" + item.text() + "</strong> from <strong>" + oldList.parent().attr('id') + "</strong> to <strong>" + newList.parent().attr('id') + "</strong>");
+		},
 		update: function(event, ui) {
-			var list    = $(this),
-				status  = list.data('status'),
-				count   = list.find('li').length,
-				badge   = list.parent().parent().find('.badge');
-
-			// update badge
-			badge.html(count);
-
 			// ajax
 			// $.ajax({
 			// 	type: "POST",
@@ -25,6 +52,6 @@ $(function() {
 			// 	}
 			// });
 	    }
-	});
+	}).disableSelection();
 
 });
